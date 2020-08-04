376690885fec441a7317545d1a7ac91fcc1ed51c
Tejal Khandelwal <c-tejal.khandelwal@ICERTIS.COM>
2020-08-04 19:10:44 +0530

    QR code Reader Console App

--- QRCodeReaderConsoleApp/Program.cs
+++ QRCodeReaderConsoleApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace QRCodeReaderConsoleApp
 {
@@ -10,6 +11,55 @@ namespace QRCodeReaderConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Path with File Name(PNG, GIF or JP(E)G) \n(E.g." + @"C:\Users\c-tejal.khandelwal\Downloads\TestTejal.png) : " );
+            var path = @"" + Console.ReadLine();
+
+            if (Directory.Exists(Path.GetDirectoryName(path)))
+            {
+                ReadQRCode(path).Wait();
+            }
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Reading QR code
+        /// </summary>
+        /// <param name="path">The file path with the file name</param>
+        /// <returns></returns>
+        static async Task ReadQRCode(string path)
+        {
+            var url = "http://api.qrserver.com/v1/read-qr-code/";
+            var baseURL = "http://api.qrserver.com/";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(baseURL);
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            var stream = new FileStream(path, FileMode.Open);
+            HttpContent content = new StreamContent(stream);
+            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            {
+                Name = "file",
+                FileName = Path.GetFileName(path),
+                
+            };
+
+            form.Add(content);
+            HttpResponseMessage response = null;
+
+            try
+            {
+                response = (client.PostAsync(url, form)).Result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("\nResult: " + result);
+            
         }
     }
 }
